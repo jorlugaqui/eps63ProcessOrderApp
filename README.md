5a6a7c0107f24709d2f8d6d922fd238c71de25e6
Jorge Galvis <jorlugaqui@gmail.com>
2019-10-06 23:41:33 -0500

    Add order's callback

--- README.md
+++ README.md
@@ -8,21 +8,21 @@ The `/process_order` endpoint will verify the stock of a product and will genera
 
 Local:
 
-`FLASK_APP=app.py flask run`
+`AWS_ACCESS_KEY=<your_aws_access_key> AWS_SECRET_ACCESS_KEY=<your_aws_secret_key> STRIPE_API_KEY=<your_stripe_api_key> FLASK_APP=app.py flask run`
 
 Go to http://localhost:5000/process_order
 
 Prod:
 
-`gunicorn --bind 0.0.0.0:8000 wsgi:app`
+`AWS_ACCESS_KEY=<your_aws_access_key> AWS_SECRET_ACCESS_KEY=<your_aws_secret_key> STRIPE_API_KEY=<your_stripe_api_key> gunicorn --bind 0.0.0.0:8000 wsgi:app`
 
 Go to http://localhost:8000/process_order
 
 Docker:
 
-`docker build -t <your-namespace>/process_order  .`
+`docker build -t <your-namespace>/process_order .`
 
-`docker run -p 8000:8000 --rm <your-namespace>/process_order`
+`docker run -p 8000:8000 -e AWS_ACCESS_KEY=<your_aws_access_key> -e AWS_SECRET_ACCESS_KEY=<your_aws_secret_key> -e STRIPE_API_KEY=<your_stripe_api_key> --rm <your-namespace>/process_order`
 
 Go to http://localhost:8000/process_order
 
--- app.py
+++ app.py
@@ -1,30 +1,69 @@
+import os
+import boto3
 import json
 import logging
 import requests
+import stripe
+import watchtower
 
-from flask import Flask, request
+from flask import Flask, request, abort
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-app = Flask(__name__)
+logs_session = boto3.Session(
+    region_name='us-east-2',
+    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY'),
+    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY')
+)
 
-@app.route('/process_order', methods = ['GET', 'POST'])
+logging.basicConfig(level=logging.INFO)
+app = Flask("loggable")
+handler = watchtower.CloudWatchLogHandler(boto3_session=logs_session)
+app.logger.addHandler(handler)
+logging.getLogger("werkzeug").addHandler(handler)
+
+
+@app.route('/process_order', methods = ['POST'])
 def process_order():
-    js = None
+    order_payload = None
     try:
-        js = json.loads(request.data)
+        order_payload = json.loads(request.data)
     except Exception as e:
-        pass
+        app.logger.error(f'Unable to parse payload {request.data}')
+        app.logger.error(e)
+        abort(400)
 
     hdr = request.headers.get('X-Amz-Sns-Message-Type')
+    
     # subscribe to the SNS topic
-    if hdr == 'SubscriptionConfirmation' and 'SubscribeURL' in js:
-        requests.get(js['SubscribeURL'])
-    
-    logger.info(js)
-    
-    return 'processed'
+    if hdr == 'SubscriptionConfirmation' and 'SubscribeURL' in order_payload:
+        try:
+            requests.get(order_payload.get('SubscribeURL'))
+        except Exception as e:
+            app.logger.error(f'Unable to confirm subscription')
+            app.logger.error(e)
+            abort(500)
 
+
+    if order_payload is None:
+        app.logger.info('No payload received')
+        return 'No payload received', 200
+
+    app.logger.info(f'Processing payment with {order_payload}')
+
+    try:
+        # generate a dummy payment intent
+        stripe.api_key = os.environ.get('STRIPE_API_KEY')
+
+        stripe.PaymentIntent.create(
+            amount=json.loads(order_payload.get('Message')).get('amount'),
+            currency='usd',
+            payment_method_types=['card']
+        )
+
+        return 'Payment processed', 200
+    except Exception as e:
+        app.logger.error(f'Unable to process the payment')
+        app.logger.error(e)
+        abort(500)
 
 
 if __name__ == '__main__':
